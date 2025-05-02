7b30f6973c95f7749e1c0e5969c71f10dddbfda5
shinhansol <wkdqhrh010@naver.com>
2025-05-02 22:54:45 +0900

    fix build fail

--- src/components/layout/SideMenu.tsx
+++ src/components/layout/SideMenu.tsx
@@ -1,4 +1,3 @@
-import { Icon } from '@iconify/react/dist/iconify.js';
 import clsx from 'clsx';
 import { AnimatePresence, motion } from 'framer-motion';
 import React, { Dispatch, SetStateAction } from 'react';
